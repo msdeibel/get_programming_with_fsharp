30edd92b1cb7be9072832b0eaed08bc2884323a6
msdeibel <git2018@markus-deibel.de>
2018-12-30 09:48:59 +0100

    Collections complete

---
+++ ConsoleApp1/201.fsx
@@ -0,0 +1,8 @@
+﻿open System
+
+let directoryDates =
+    System.IO.Directory.EnumerateDirectories "C:\\"
+    |> Seq.map(fun d -> System.IO.DirectoryInfo(d))
+    |> Seq.map(fun di -> (di.Name, di.CreationTimeUtc))
+    |> Map.ofSeq
+    |> Map.map (fun k v -> printfn "%s - %s" k (DateTime.UtcNow.Subtract(v).ToString()))
