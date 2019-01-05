467058064a88fbd326ad71e7803fbedb3877fe42
msdeibel <git2018@markus-deibel.de>
2019-01-05 08:38:21 +0100

    Folding

---
+++ ConsoleApp1/210.fsx
@@ -0,0 +1,53 @@
+﻿let sum inputs =
+    Seq.fold   
+        (fun state input ->
+            let newState = state + input
+            printfn 
+                "Current state is %d, input is %d, new state value is %d"
+                state
+                input
+                newState
+            newState)
+        0
+        inputs
+
+sum [1..5]
+
+let length inputs =
+    Seq.fold   
+        (fun state input ->
+            let newState = state + 1
+            printfn 
+                "Current state is %d, input has one more item, new state value is %d"
+                state
+                newState
+            newState)
+        0
+        inputs
+
+length [1..5]
+
+let max inputs =
+    Seq.fold   
+        (fun state input ->
+            let newState = 
+                if input > state then
+                    input
+                else
+                    state    
+            printfn 
+                "Current state is %d, input is %d, new state value is %d"
+                state
+                input
+                newState
+            newState)
+        0
+        inputs
+
+max [1;5;2;4;7;6;9]
+
+let inputs = [1..9]
+
+Seq.fold (fun state input -> state + input) 0 inputs
+inputs |> Seq.fold (fun state input -> state + input) 0
+(0, inputs) ||> Seq.fold (fun state input -> state + input)
---
+++ ConsoleApp1/215.fsx
@@ -0,0 +1,50 @@
+﻿open System
+type Rule = string -> bool * string
+
+let rules : Rule list =
+    [   
+        fun text -> 
+            printfn "Three word rule"
+            (text.Split ' ').Length = 3, "Must be three words"
+        fun text -> 
+            printfn "30 character rule"
+            text.Length <= 30, "Max length is 30 characters"
+        fun text -> 
+            printfn "All caps rule"        
+            text
+            |> Seq.filter Char.IsLetter
+            |> Seq.forall Char.IsUpper, "All letters must be caps" 
+        fun text -> 
+            printfn "No numbers rule"
+            text
+            |> Seq.filter Char.IsNumber
+            |> Seq.length = 0, "Must not contain numbers" 
+    ]
+
+let validateManual (rules: Rule list) word =
+    let passed, error = rules.[0] word
+    if not passed then false, error
+    else    
+        let passed, error = rules.[1] word
+        if not passed then false, error
+        else    
+            let passed, error = rules.[2] word
+            if not passed then false, error
+            else
+                true,""
+
+let buildValidator (rules: Rule list) =
+    rules
+    |> List.reduce(fun firstRule secondRule ->
+        fun word ->
+            let passed, error = firstRule word
+            if passed then  
+                let passed, error = secondRule word
+                if passed then true, ""
+                else false, error
+            else false, error)
+
+let validate = buildValidator rules
+let word = "HELLO FROM FSHARP4"
+
+validate word
