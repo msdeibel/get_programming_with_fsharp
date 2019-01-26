5daa1e4777c1a658375ad45512ac3b0a47f88714
msdeibel <git2018@markus-deibel.de>
2019-01-26 07:45:36 +0100

    Pattern matching

---
+++ ConsoleApp1/237.fsx
@@ -0,0 +1,21 @@
+﻿let getCreditLimit customer =
+    match customer with
+    | "medium", 1 -> 500
+    | "good", 0 | "good", 1 -> 750
+    | "good", 2 -> 1000
+    | "good", _ -> 2000
+    | _ -> 250
+
+getCreditLimit ("Bad", 0)
+
+let getLimitNested customer =
+    match customer with
+    | "medium", 1 -> 500
+    | "good", years ->
+        match years with
+        | 0 | 1 -> 750
+        | 2 -> 1000
+        | _ -> 2000
+    | _ -> 250
+
+getLimitNested ("good", 2)
---
+++ ConsoleApp1/240.fsx
@@ -0,0 +1,16 @@
+﻿type Customer =
+    {
+        Name: string
+        Balance: int
+    }
+
+let handleCustomers customers =
+    match customers with
+    | [] -> failwith "No customers supplied!"
+    | [customer] -> printfn "Single customer, name is %s" customer.Name
+    | [first; second] -> printfn "Two customers, balance = %d" (first.Balance + second.Balance)
+    | customers -> printfn "Customers supplied: %d" customers.Length
+
+
+handleCustomers [{Balance = 10; Name= "Joe"}]
+//handleCustomers []
