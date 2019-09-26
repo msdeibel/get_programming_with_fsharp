01d37a2c8601c13bd812f389c2a7dcc5114648ed
Markus <emailaddress@markus-deibel.de>
2019-09-26 06:32:09 +0200

    Pattern matching for tryFindAccountFolder

--- Capstone4/FileRepository.fs
+++ Capstone4/FileRepository.fs
@@ -8,12 +8,21 @@ let private accountsPath =
     let path = @"accounts"
     Directory.CreateDirectory path |> ignore
     path
+
 let private findAccountFolder owner =    
     let folders = Directory.EnumerateDirectories(accountsPath, sprintf "%s_*" owner)
     if Seq.isEmpty folders then ""
     else
         let folder = Seq.head folders
         DirectoryInfo(folder).Name
+
+let private tryFindAccountFolder owner =    
+    let folders = Directory.EnumerateDirectories(accountsPath, sprintf "%s_*" owner) |> Seq.toList
+    match folders.Length with
+    | 0 -> None
+    | _ -> Some(Seq.head folders)
+
+
 let private buildPath(owner, accountId:Guid) = sprintf @"%s\%s_%O" accountsPath owner accountId
 
 let loadTransactions (folder:string) =
@@ -24,11 +33,11 @@ let loadTransactions (folder:string) =
                       |> Directory.EnumerateFiles
                       |> Seq.map (File.ReadAllText >> Transactions.deserialize)
 
-/// Finds all transactions from disk for specific owner.
 let findTransactionsOnDisk owner =
-    let folder = findAccountFolder owner
-    if String.IsNullOrEmpty folder then owner, Guid.NewGuid(), Seq.empty
-    else loadTransactions folder
+    let folder = tryFindAccountFolder owner
+    match folder with
+    | Some(_string) -> Some(loadTransactions folder.Value)
+    | _ -> None
 
 /// Logs to the file system
 let writeTransaction accountId owner transaction =
