0d882adb75b2c44d42836ccf241a05c50d73f1fe
msdeibel <git2018@markus-deibel.de>
2019-10-31 19:09:59 +0100

    Up to 26_1_5

---
+++ NugetFSharp/NugetFSharp/Script1.fsx
@@ -0,0 +1,15 @@
+﻿#I @"C:\Users\Markus\.nuget\packages"
+#r @"newtonsoft.json\12.0.2\lib\netstandard2.0\Newtonsoft.Json.dll"
+#r @"humanizer.core\2.7.9\lib\netstandard2.0\Humanizer.dll"
+
+open Humanizer
+
+"ScriptsAreAGreatWayToExplorePackages".Humanize()
+
+"ScriptsAreAGreatWayToExplorePackages".Humanize(LetterCasing.LowerCase)
+
+
+
+#load "Library1.fs"
+Library1.getPerson()
+
