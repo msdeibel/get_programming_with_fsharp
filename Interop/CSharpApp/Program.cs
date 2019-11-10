c273a8da32bcdbbd1be9ebc26d0ef939bd9d728b
msdeibel <git2018@markus-deibel.de>
2019-11-10 09:07:57 +0100

    27.1

---
+++ Interop/CSharpApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Model;
+
+namespace CSharpApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var car = new Car(6, "GMC", Tuple.Create(1.5, 3.5));
+
+            var wheels = car.Wheels;
+
+            var width = car.Dimensions.Item1;
+
+            var v1000 = Vehicle.NewMotorbike("Versys", 1000.0);
+
+            var name = (v1000 as Vehicle.Motorbike).Name;
+        }
+    }
+}
---
+++ Interop/FSharpCode/Library.fs
@@ -0,0 +1,20 @@
+﻿namespace Model
+
+type Car = 
+    {
+        /// Number of wheels on the car
+        Wheels : int
+
+        /// Brand of the car
+        Brand : string
+
+        /// The x,y dimensions of the car in meters
+        Dimensions : float * float
+    }
+
+type Vehicle = 
+      /// A car is a type of vehicle
+    | Motorcar of Car
+      /// <summary>A motorbike is another type of vehicle</summary>
+    | Motorbike of Name:string * EngineSize:float
+
