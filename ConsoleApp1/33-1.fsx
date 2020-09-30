7f63155c9f6498a4a3100b36dcf5dc527c1a4b0e
msdeibel <git2018@markus-deibel.de>
2020-09-30 07:12:14 +0200

    33-1 first refacktoring

---
+++ ConsoleApp1/33-1.fsx
@@ -0,0 +1,25 @@
+﻿#I @"C:\Users\Markus\.nuget\packages"
+#r @"fsharp.data\3.0.0\lib\netstandard2.0\FSharp.Data.dll"
+
+open FSharp.Data
+
+type Package = 
+    FSharp.Data.HtmlProvider< @"..\data\sample-package.html" >
+
+let getPackage packageName =
+    packageName 
+    |> sprintf "https://www.nuget.org/packages/%s" 
+    |> Package.Load
+
+let getDownloadsForPackage packageName =
+    let package = getPackage packageName
+    package.Tables.``Version History``.Rows
+    |> Seq.sumBy(fun p -> p.Downloads)
+
+let getDetailsForVersion packageName version =
+    let package = getPackage packageName
+    package.Tables.``Version History``.Rows
+    |> Seq.tryFind(fun p -> p.Version.Contains version)
+
+getDownloadsForPackage "EntityFramework"
+getDetailsForVersion "EntityFramework" "6.1.0"
